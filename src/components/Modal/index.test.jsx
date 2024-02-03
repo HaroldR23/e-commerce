a84248bd023cbc9bab1f86a2ebd939a83ebf49b6
HaroldR23 <harold.rodriguez@ioet.com>
2024-02-03 20:24:48 -0300

    test: implement test for Modal component

---
+++ src/components/Modal/index.test.jsx
@@ -0,0 +1,27 @@
+import { Modal } from '.';
+import { mockValue } from '../../contexts/SearchContext/MockSearchContext';
+import { render, screen } from '@testing-library/react';
+import { SearchContext } from '../../contexts/SearchContext';
+describe("Modal", () => {
+    it("It should render a modal container content based on the context", () => {
+        render(
+            <SearchContext.Provider value={
+                {
+                    ...mockValue, 
+                    titleProduct: "Title test product",
+                    priceProduct: "100",
+                    descriptionProduct: "Test description",
+                } 
+            }
+            >
+                <Modal/>
+            </SearchContext.Provider>
+        );
+        const titleTextContent = screen.getByText("Title test product");
+        const descriptionContent = screen.getByText("Test description");
+        const priceContent = screen.getByText("$100");
+        expect(priceContent).toBeInTheDocument;
+        expect(titleTextContent).toBeInTheDocument;
+        expect(descriptionContent).toBeInTheDocument;
+    });
+});
---
+++ src/components/Modal/indexConfig.js
@@ -0,0 +1,20 @@
+const mockSetFunction = jest.fn();
+
+export const mockValue = {
+    searchValue: "",
+    setSearchValue: mockSetFunction,
+    searchedProducts: [],
+    isLoading: true,
+    isOpen: false,
+    setIsOpen: mockSetFunction,
+    imageProduct: "",
+    setImageProduct: mockSetFunction,
+    titleProduct: "",
+    setTitleProduct: mockSetFunction,
+    priceProduct: "",
+    setPriceProduct: mockSetFunction,
+    descriptionProduct: "",
+    setDescriptionProduct: mockSetFunction,
+    rateProduct: 0,
+    setRateProduct: mockSetFunction
+};
