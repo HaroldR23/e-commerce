7d7e654734db55f58504ff3a0270c47ed2c00fd9
HaroldR23 <harold.rodriguez@ioet.com>
2024-02-05 13:07:08 -0300

    fix: fix products card size

--- src/components/ResultTable/Card/Detail/index.jsx
+++ src/components/ResultTable/Card/Detail/index.jsx
@@ -5,7 +5,7 @@ import './Detail.css';
 function Detail ({title, price, rate }) {
     return (
         <div className="DetailsCardContainer">
-            <h3>{title}</h3>
+            <h3 id="titleContent">{title}</h3>
             <Rating stars={rate}/>
             <h3>${price}</h3>
         </div>
